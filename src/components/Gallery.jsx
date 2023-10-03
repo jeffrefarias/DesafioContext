29e5d1c79b697ba3c07578ab1ea8b0fcfdde761e
Jeffrey Farías <jeffreyfarias5@gmail.com>
2023-10-02 23:30:21 -0300

    basura

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,33 +1,30 @@
-// Uso de useContext en componente Gallery (punto 4 del desafío)
+
 import { useContext } from 'react';
 import { MyContext } from '../MyContext';
-import  Card  from 'react-bootstrap/Card';
+import Card from 'react-bootstrap/Card';
 
 import IconHeart from '../components/IconHeart';
 
 const Gallery = () => {
-  const {fotos, setFotos} = useContext(MyContext); // Uso de useContext (punto 4)
+  const { fotos, setFotos } = useContext(MyContext);
 
   const addFavorito = (id) => {
     const fotoIndex = fotos.findIndex(e => e.id === id);
-    // cambia el valor de liked de false a true y viceversa
     fotos[fotoIndex].liked = !fotos[fotoIndex].liked;
-    setFotos([... fotos]);
+    setFotos([...fotos]);
   }
 
   return <div className="gallery grid-columns-5 p-3">
     {
       fotos.map((foto, i) =>
         <Card key={i} className='text-white' onClick={() => addFavorito(foto.id)}>
-          <Card.Img src={ foto.src.tiny } alt={ foto.alt } />
-          <Card.ImgOverlay className='mi-estilo'>
-            <Card.Title className='text-end'>
-              <IconHeart filled={ foto.liked } />
-            </Card.Title>
-            <Card.Footer className='text-white text-start '>
-              { foto.alt }
-            </Card.Footer>
-          </Card.ImgOverlay>
+          <Card.Img src={foto.src.tiny} alt={foto.alt} />
+          <div className='IconHeart'>
+            <IconHeart filled={foto.liked} />
+          </div>
+          <Card.Footer className='text-white text-start '>
+            {foto.alt}
+          </Card.Footer>
         </Card>
       )
     }
--- src/views/Favorites.jsx
+++ src/views/Favorites.jsx
@@ -1,9 +1,40 @@
+
+import { useContext } from "react";
+import { MyContext } from '../MyContext';
+import Card from 'react-bootstrap/Card';
+
 const Favorites = () => {
+  const { fotos, setFotos } = useContext(MyContext);
+
+  const borrar = (id) => {
+    const fotoIndex = fotos.fotoIndex(e => e.id === id);
+
+    fotos[fotoIndex].liked = !fotos[fotoIndex].liked;
+    setFotos([...fotos]);
+  }
+
+
   return (
-    <div>
+    <div className="App">
       <h1>Fotos favoritas</h1>
-      <div className="p-3 gallery grid-columns-4"></div>
+      <div className="p-3 gallery grid-columns-4">
+        {
+          fotos.filter(e => e.liked).map((foto, i) =>
+            <Card key={i} className='text-white' onClick={() => borrar()}>
+              <Card.Img src={foto.src.tiny} alt={foto.alt} />
+              <Card.ImgOverlay>
+                <Card.Title className='text-end'>
+                </Card.Title>
+                <Card.Footer className='text-white text-start '>
+                  {foto.alt}
+                </Card.Footer>
+              </Card.ImgOverlay>
+            </Card>
+          )
+        }
+      </div>
     </div>
   );
 };
+
 export default Favorites;
